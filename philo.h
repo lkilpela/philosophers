86f795d0a68b010589eb06b8dca85b45f6410b60
lumikil <kilpelainenlumi@gmail.com>
2024-04-23 21:01:25 +0300

    u

---
+++ philo.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 20:55:55 by lkilpela          #+#    #+#             */
+/*   Updated: 2024/04/23 21:01:11 by lkilpela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+// pthread_create, pthread_detach, pthread_join, pthread_mutex_init,
+// pthread_mutex_destroy, pthread_mutex_lock, pthread_mutex_unlock
+# include <pthread.h>
+# include <stdio.h>     // printf
+//# include <limits.h>
+# include <unistd.h>    // write, usleep
+# include <stdlib.h>    // malloc, free
+# include <sys/time.h>  // gettimeofday, 
+# include <string.h>    // memset
+
+
+#endif
