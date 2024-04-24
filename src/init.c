0bf3c164b9643dbe5037949ffc94e195c93ad018
lumikil <kilpelainenlumi@gmail.com>
2024-04-24 22:28:13 +0300

    Update comments in init.c file

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 15:22:07 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/24 22:24:19 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/24 22:27:58 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,3 +71,22 @@ int	init_philo(t_program *p)
 			return (1);
 	}
 }
+/*
+The pthread_create function in C is used to create a new thread.
+It has the following parameters:
+
+pthread_t *thread: A pointer to a pthread_t variable that the function will fill
+					with the ID of the newly created thread.
+
+const pthread_attr_t *attr: A pointer to a pthread_attr_t structure 
+							that specifies thread attributes, 
+							or NULL for the default attributes.
+
+void *(*start_routine) (void *): A pointer to the function that the 
+								new thread will start executing. 
+								This function should take a single void * arg
+								and return a void *.
+
+void *arg: A pointer to the argument that will be passed to 
+			the start_routine function.
+*/
