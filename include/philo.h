2f928cd459297ac35b2b60031eee58e26ec063e0
lumikil <kilpelainenlumi@gmail.com>
2024-04-26 09:13:20 +0300

    Update quit variable initialization in init_program function

--- include/philo.h
+++ include/philo.h
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 20:55:55 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/26 08:49:33 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/26 09:13:17 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,6 @@ typedef struct	s_program
 	pthread_mutex_t		*forks;
 	t_philo				*philo;
 	pthread_mutex_t		lock;
-	int					quit;
 }				t_program;
 //init
 int			init_program(t_program *p, int ac, char **av);
