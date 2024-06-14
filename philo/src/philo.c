4fd80a58b3278abca37f7346319d0deddc07fbea
lkilpela <lkilpela@student.hive.fi>
2024-06-14 11:09:48 +0300

    u

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 12:07:58 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/06/14 11:08:33 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/06/14 11:09:46 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,13 @@ void	wait_for_philos(t_program *p)
 	}
 }
 
-int	main(int ac, char **av)
+int	main(int argc, char **argv)
 {
 	t_program	p;
 
-	if (ac < 5 || argc > 6)
+	if (argc < 5 || argc > 6)
 		return (printf("Wrong argument count\n"), 1);
-	if (init_program(&p, ac, av) == 1)
+	if (init_program(&p, argc, argv) == 1)
 		return (1);
 	if (p.num_of_philos == 1)
 		printf("%lld %d %s\n", p.time_to_die, 1, RED "died" NC);
